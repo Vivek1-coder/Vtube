d0e5ba89dc15d8f676cc1ec1374562232b265491
Vivek1-coder <vy212205@gmail.com>
2024-12-14 18:03:07 +0530

    routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser,loginUser,logoutUser,refreshAccessToken} from "../controllers/user.controller.js";
+import {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails, updateUserAvatar,updateUserCoverImage,userChannelProfile,getWatchHistory} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -28,5 +28,15 @@ userRouter.route("/login").post(loginUser)
 //secured routes
 userRouter.route("/logout").post(verifyJWT, logoutUser) // next ki vajah se verify vale ke bad logout pr aa jaega
 userRouter.route("/refresh-token").post(refreshAccessToken) //sara kam hamne usi ke andar kiya h so no need of verifyjwt
+userRouter.route("/change-password").post(verifyJWT,changeCurrentPassword)
+userRouter.route("/current-user").get(verifyJWT,getCurrentUser)
+userRouter.route("/update-account").patch(verifyJWT,updateAccountDetails) //patch for updating
+ 
+userRouter.route("/avatar-update").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+userRouter.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+userRouter.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+userRouter.route("/history").get(verifyJWT,getWatchHistory)
+
 
 export default userRouter
